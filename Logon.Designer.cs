6d89cf9cda371ba93775b1277fef0510d54f155c
Stewart Million-Perez <stewart.millionperez@und.edu>
2022-04-23 17:01:40 -0600

    updated logon/password combo

--- Logon.Designer.cs
+++ Logon.Designer.cs
@@ -29,32 +29,37 @@ namespace WindowsFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Logon));
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.username_text = new System.Windows.Forms.TextBox();
+            this.password_text = new System.Windows.Forms.MaskedTextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.LOGIN_exc = new System.Windows.Forms.Button();
+            this.Reg_new_user_exc = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // username_text
             // 
-            this.textBox1.Location = new System.Drawing.Point(159, 30);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(169, 20);
-            this.textBox1.TabIndex = 0;
+            this.username_text.Location = new System.Drawing.Point(159, 30);
+            this.username_text.Name = "username_text";
+            this.username_text.Size = new System.Drawing.Size(169, 20);
+            this.username_text.TabIndex = 0;
+            this.username_text.Validating += new System.ComponentModel.CancelEventHandler(this.username_text_Validating);
             // 
-            // maskedTextBox1
+            // password_text
             // 
-            this.maskedTextBox1.Location = new System.Drawing.Point(159, 58);
-            this.maskedTextBox1.Name = "maskedTextBox1";
-            this.maskedTextBox1.Size = new System.Drawing.Size(169, 20);
-            this.maskedTextBox1.TabIndex = 1;
+            this.password_text.Location = new System.Drawing.Point(159, 58);
+            this.password_text.Name = "password_text";
+            this.password_text.PasswordChar = '*';
+            this.password_text.Size = new System.Drawing.Size(169, 20);
+            this.password_text.TabIndex = 1;
             // 
             // label1
             // 
@@ -76,30 +81,31 @@ namespace WindowsFormsApp1
             this.label2.TabIndex = 3;
             this.label2.Text = "PASSWORD";
             // 
-            // button1
+            // LOGIN_exc
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button1.Location = new System.Drawing.Point(130, 90);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(92, 31);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "LOGIN";
-            this.button1.UseVisualStyleBackColor = false;
+            this.LOGIN_exc.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.LOGIN_exc.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LOGIN_exc.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.LOGIN_exc.Location = new System.Drawing.Point(130, 90);
+            this.LOGIN_exc.Name = "LOGIN_exc";
+            this.LOGIN_exc.Size = new System.Drawing.Size(92, 31);
+            this.LOGIN_exc.TabIndex = 4;
+            this.LOGIN_exc.Text = "&LOGIN";
+            this.LOGIN_exc.UseVisualStyleBackColor = false;
+            this.LOGIN_exc.Click += new System.EventHandler(this.LOGIN_exc_Click);
             // 
-            // button2
+            // Reg_new_user_exc
             // 
-            this.button2.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.SystemColors.Window;
-            this.button2.Location = new System.Drawing.Point(254, 409);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(155, 38);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "REGISTER NEW USER";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.Reg_new_user_exc.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.Reg_new_user_exc.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Reg_new_user_exc.ForeColor = System.Drawing.SystemColors.Window;
+            this.Reg_new_user_exc.Location = new System.Drawing.Point(254, 409);
+            this.Reg_new_user_exc.Name = "Reg_new_user_exc";
+            this.Reg_new_user_exc.Size = new System.Drawing.Size(155, 38);
+            this.Reg_new_user_exc.TabIndex = 5;
+            this.Reg_new_user_exc.Text = "&REGISTER NEW USER";
+            this.Reg_new_user_exc.UseVisualStyleBackColor = false;
+            this.Reg_new_user_exc.Click += new System.EventHandler(this.button2_Click);
             // 
             // pictureBox1
             // 
@@ -113,10 +119,10 @@ namespace WindowsFormsApp1
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.maskedTextBox1);
-            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.password_text);
+            this.groupBox2.Controls.Add(this.username_text);
             this.groupBox2.Controls.Add(this.label1);
-            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.LOGIN_exc);
             this.groupBox2.Controls.Add(this.label2);
             this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox2.Location = new System.Drawing.Point(155, 274);
@@ -126,6 +132,10 @@ namespace WindowsFormsApp1
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "START SMASHING!";
             // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
             // Logon
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -133,27 +143,29 @@ namespace WindowsFormsApp1
             this.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Reg_new_user_exc);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Logon";
             this.Text = "Login To BugSmasher";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.TextBox username_text;
+        private System.Windows.Forms.MaskedTextBox password_text;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button LOGIN_exc;
+        private System.Windows.Forms.Button Reg_new_user_exc;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
     }
 }
 
--- Logon.cs
+++ Logon.cs
@@ -16,16 +16,70 @@ namespace WindowsFormsApp1
 
         public static string username;
         public static int user_id;
-
+        public static string role;
+        SQLiteConnection sql_lite_connection = new SQLiteConnection("Data Source = 463.db");
 
         public Logon()
         {
             InitializeComponent();
+            
 
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+
+        }
+
+        private void LOGIN_exc_Click(object sender, EventArgs e)
+        {
+            sql_lite_connection.Open();
+            SQLiteCommand user_pword_valid = new SQLiteCommand(sql_lite_connection);
+            user_pword_valid.CommandText = "SELECT EXISTS (SELECT * FROM User_Logon WHERE Username = @p1 AND Password = @p2";
+            user_pword_valid.CommandType = CommandType.Text;
+            user_pword_valid.Parameters.Add(new SQLiteParameter("@p1", username_text.Text));
+            user_pword_valid.Parameters.Add(new SQLiteParameter("@p2", password_text.Text));
+
+            string valid = user_pword_valid.ExecuteScalar().ToString();
+            sql_lite_connection.Close();
+
+            if (valid == "true")
+            {
+                Logon.username = username_text.Text;
+                sql_lite_connection.Open();
+                // Find User ID
+                var staff_id_selectstatement = $"SELECT User_ID FROM User_Logon WHERE Username = '{Logon.username}'";
+                SQLiteCommand staffID = new SQLiteCommand(staff_id_selectstatement, sql_lite_connection);
+                Logon.user_id = Convert.ToInt32(staffID.ExecuteScalar());
+                // Find User Role
+                var staff_role_selectstatemetn = $"SELECT Role FROM User_Information WHERE User_ID = '{Logon.username}'";
+                SQLiteCommand staffRole = new SQLiteCommand(staff_role_selectstatemetn, sql_lite_connection);
+                Logon.role = staffRole.ExecuteScalar().ToString();
+
+            }
+
+            if (ValidateChildren(ValidationConstraints.Enabled))
+            {
+                MessageBox.Show(username_text.Text, "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+
+
+        }
+
+        private void username_text_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrEmpty(username_text.Text))
+            {
+                e.Cancel = true;
+                username_text.Focus();
+                errorProvider1.SetError(username_text, "Please enter your username");
+            }
+            else
+            {
+                e.Cancel = false;
+                errorProvider1.SetError(username_text, null);
+            }
 
         }
     }
