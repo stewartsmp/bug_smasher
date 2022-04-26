a367604652657770625c1abbf30274296969f67f
Stewart Million-Perez <stewart.millionperez@und.edu>
2022-04-26 09:54:40 -0600

    updated app

--- Logon.cs
+++ Logon.cs
@@ -33,38 +33,59 @@ namespace WindowsFormsApp1
 
         private void LOGIN_exc_Click(object sender, EventArgs e)
         {
-            sql_lite_connection.Open();
-            SQLiteCommand user_pword_valid = new SQLiteCommand(sql_lite_connection);
-            user_pword_valid.CommandText = "SELECT EXISTS (SELECT * FROM User_Logon WHERE Username = @p1 AND Password = @p2";
-            user_pword_valid.CommandType = CommandType.Text;
-            user_pword_valid.Parameters.Add(new SQLiteParameter("@p1", username_text.Text));
-            user_pword_valid.Parameters.Add(new SQLiteParameter("@p2", password_text.Text));
+            var blank_text_field = "Please enter the requested information";
 
-            string valid = user_pword_valid.ExecuteScalar().ToString();
-            sql_lite_connection.Close();
-
-            if (valid == "true")
+            if (String.IsNullOrEmpty(username_text.Text) || String.IsNullOrEmpty(password_text.Text))
             {
-                Logon.username = username_text.Text;
+                MessageBox.Show(blank_text_field,"Enter username or password", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            else
+            {
                 sql_lite_connection.Open();
-                // Find User ID
-                var staff_id_selectstatement = $"SELECT User_ID FROM User_Logon WHERE Username = '{Logon.username}'";
-                SQLiteCommand staffID = new SQLiteCommand(staff_id_selectstatement, sql_lite_connection);
-                Logon.user_id = Convert.ToInt32(staffID.ExecuteScalar());
-                // Find User Role
-                var staff_role_selectstatemetn = $"SELECT Role FROM User_Information WHERE User_ID = '{Logon.username}'";
-                SQLiteCommand staffRole = new SQLiteCommand(staff_role_selectstatemetn, sql_lite_connection);
-                Logon.role = staffRole.ExecuteScalar().ToString();
+                SQLiteCommand user_pword_valid = new SQLiteCommand(sql_lite_connection);
+                user_pword_valid.CommandText = "SELECT EXISTS (SELECT * FROM User_Logon WHERE Username = @p1 AND Password = @p2);";
+                user_pword_valid.CommandType = CommandType.Text;
+                user_pword_valid.Parameters.Add(new SQLiteParameter("@p1", username_text.Text));
+                user_pword_valid.Parameters.Add(new SQLiteParameter("@p2", password_text.Text));
 
+                int valid = Convert.ToInt32(user_pword_valid.ExecuteScalar());
+                sql_lite_connection.Close();
+                .
+
+                if (valid == 1)
+                {
+                    // Assign Username
+                    Logon.username = username_text.Text;
+                    sql_lite_connection.Open();
+                    // Assign User ID
+                    var staff_id_selectstatement = $"SELECT User_ID FROM User_Logon WHERE Username = '{Logon.username}'";
+                    SQLiteCommand staffID = new SQLiteCommand(staff_id_selectstatement, sql_lite_connection);
+                    Logon.user_id = Convert.ToInt32(staffID.ExecuteScalar());
+                    // Assign User Role
+                    var staff_role_selectstatemetn = $"SELECT Role_Description FROM User_Information WHERE User_ID = {Logon.user_id}";
+                    SQLiteCommand staffRole = new SQLiteCommand(staff_role_selectstatemetn, sql_lite_connection);
+                    Logon.role = staffRole.ExecuteScalar().ToString();
+                    sql_lite_connection.Close();
+                    // Switch between views based on role
+                    switch (Logon.role)
+                    {
+                        case "Developer":
+                            Console.WriteLine(Logon.username + " is a Developer");
+                            break;
+                        case "Manager":
+                            Console.WriteLine(Logon.username + "is a Manager");
+                            break;
+
+                    }
+
+
+                }
+                else
+                {
+                    MessageBox.Show(username_text.Text, "Invalid username or password", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
             }
-
-            if (ValidateChildren(ValidationConstraints.Enabled))
-            {
-                MessageBox.Show(username_text.Text, "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-
-
-
         }
 
         private void username_text_Validating(object sender, CancelEventArgs e)
